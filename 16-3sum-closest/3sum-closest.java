284ed16799777ab2e00e2dc3af036b1984d2a679
LOGAVARSHINI P <logavarshini2112@gmail.com>
2026-09-14 18:03:38 +0530

    Time: 17 ms (77.16%) | Memory: 45.4 MB (66.73%) - LeetSync

--- 16-3sum-closest/3sum-closest.java
+++ 16-3sum-closest/3sum-closest.java
@@ -4,7 +4,7 @@ class Solution {
         int closest = nums[0]+nums[1]+nums[2];
         int sum=0;
         Arrays.sort(nums);
-        for(int i=0; i<=n-2; i++){
+        for(int i=0; i<n-2; i++){
             int l=i+1;
             int r= n-1;
             while(l<r){
